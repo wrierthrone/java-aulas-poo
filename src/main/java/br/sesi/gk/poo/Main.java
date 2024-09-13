e713e1aba008a2e67458b19d421592c03ecfcdcd
Gustavo Kowalschuk <gustavo.kowalschuk@gmail.com>
2024-09-13 11:31:00 -0300

    associando valores a objetos

--- src/main/java/br/sesi/gk/poo/Main.java
+++ src/main/java/br/sesi/gk/poo/Main.java
@@ -1,9 +1,27 @@
 package br.sesi.gk.poo;
 
+import br.sesi.gk.poo.cabeca.Cabeca;
+import br.sesi.gk.poo.cabeca.Olho;
+
 public class Main {
     public static void main(String[] args) {
+
+        Olho olho1 = new Olho();
+        olho1.cego = true;
+        olho1.diametroOlho = 2.0f;
+        olho1.corIris = "azul";
+
+        Olho olho2 = new Olho();
+        olho2.cego = false;
+        olho2.diametroOlho = 2.5f;
+        olho2.corIris = "castanho";
+
+        Cabeca cabeca = new Cabeca();
+        cabeca.olhoEsquerdo = olho1;
+        cabeca.olhoDireito = olho2;
+
         Pessoa pessoa1 = new Pessoa();
 
-        pessoa1.cabeca.olho.corIris = "catanho";
+        pessoa1.cabeca = cabeca;
     }
 }
--- src/main/java/br/sesi/gk/poo/cabeca/Cabeca.java
+++ src/main/java/br/sesi/gk/poo/cabeca/Cabeca.java
@@ -1,13 +1,15 @@
 package br.sesi.gk.poo.cabeca;
 
 public class Cabeca {
-    public Olho olho;
+    public Olho olhoEsquerdo;
+    public Olho olhoDireito;
     public Orelha orelha;
     public Nariz nariz;
     public Boca boca;
 
     public Cabeca(){
-        this.olho = new Olho();
+        this.olhoEsquerdo = new Olho();
+        this.olhoDireito = new Olho();
         this.orelha = new Orelha();
         this.nariz = new Nariz();
         this.boca = new Boca();
